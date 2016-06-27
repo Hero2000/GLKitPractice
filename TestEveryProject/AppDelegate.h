06f6a0fcc83b5dab9a516552e278f945e101a6a4
yyb <18511789075@163.com>
2016-06-27 11:29:55 +0800

    Initial Commit

---
+++ TestEveryProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestEveryProject
+//
+//  Created by shawnfeng on 16/6/27.
+//  Copyright © 2016年 shawnfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestEveryProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestEveryProject
+//
+//  Created by shawnfeng on 16/6/27.
+//  Copyright © 2016年 shawnfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
